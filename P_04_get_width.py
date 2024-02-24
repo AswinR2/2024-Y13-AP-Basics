34732f6877d68a94b3b7af0444158181cb5d439f
AswinR2 <91292605+AswinR2@users.noreply.github.com>
2024-02-24 21:12:24 +1300

    Basic Width Form

---
+++ P_04_get_width.py
@@ -0,0 +1,17 @@
+# Ask user for widht and loop untill they enter a number that is more than zero
+
+error = "Please enter a number that is more than zero\n"
+while True:
+
+    try:
+        # asking for input
+        width = float(input("Width: "))
+
+        # checking that the num is > 0
+        if width > 0:
+            break
+        else:
+            print("error")
+
+    except ValueError:
+        print(error)
