d8d1d97d6237af6d41eac0782a339d4872c2af85
AswinR2 <91292605+AswinR2@users.noreply.github.com>
2024-02-24 20:32:38 +1300

    Update P_02_variables.py

--- P_02_variables.py
+++ P_02_variables.py
@@ -8,4 +8,10 @@ fav_num = 7
 num_string = "5"
 
 print(fav_num * 2)
-print(num_string*5)
+print(num_string * 5)
+
+# Constants are uppercase
+PI = 3.14
+
+# Starting should start with lower case letters and not num or upper case 
+a_01_my_num = 7
